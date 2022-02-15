04de664f5cdcbb482a258153a9c004569e26dd16
alfiarisandi <alfyarissandy@gmail.com>
2022-02-15 18:01:55 +0700

    delete comment

--- app.py
+++ app.py
@@ -1,26 +1,3 @@
-# import cv2
-# import numpy as np
-# from matplotlib import pyplot as plt
-
-# imggambar= cv2.imread("download.jpg",1)
-# cv2.imshow('hasil',imggambar)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows
-
-# height, width = imggambar.shape[:2]
-# qheight, qwidth = height / 4, width / 4
-# T = np.float32([[1, 0, qwidth], [0, 1, qheight]])
-# img_translation = cv2.warpAffine(imggambar, T, (width, height))
-# tampil_hor=np.concatenate((imggambar,img_translation),axis=1)
-# cv2.imshow('hasil',tampil_hor)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows
-
-# imgmedian = cv2.medianBlur(imggambar,15)
-# tampil_hor=np.concatenate((imggambar,imgmedian),axis=0)
-# cv2.imshow('hasil',tampil_hor)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows
 
 import cv2
 import matplotlib
